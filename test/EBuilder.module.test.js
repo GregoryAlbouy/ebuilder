cfb26fa902fb2ab3301bfe10ce10331f26a6bbab
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-05-23 21:03:02 +0200

    remove file

--- test/EBuilder.module.test.js
+++
@@ -1,7 +0,0 @@
-// const EBuilder = require('ebuilder-js')
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
-const { window } = new JSDOM()
-const EBuilder = require('ebuilder-js')
-
-console.log(EBuilder('div'))
